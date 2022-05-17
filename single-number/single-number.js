74f4164b8d45f8ded03d2cac1aa5157ac5e44320
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-17 18:15:03 -0400

    Time: 139 ms, Memory: 52.4 MB - LeetHub

---
+++ single-number/single-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    //this will host our table of numbers ex. {1000 : 5} - number 1000 appears 5 times
+    const hash = {}
+    
+    for (const num of nums) {
+        //this creates a  count of our hash table - if a number appears, +1 will increase the number once - however, if a new number appears such as 1001 - the || 1 will now create a new count
+
+        hash[num] = hash[num] +1 || 1
+    }
+    
+    //if our value in our ht only appears once(1) then we will return it
+    for (const value in hash) {
+        if (hash[value] === 1 ) {
+            console.log('here is our hash ', hash)
+            return value
+        }
+    }
+};
