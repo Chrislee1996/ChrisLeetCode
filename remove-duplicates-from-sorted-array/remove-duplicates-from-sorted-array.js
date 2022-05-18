939b4b6b587f4cca3e3d98e20bc1183bf86fc407
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-18 19:47:41 -0400

    Time: 74 ms, Memory: 44.1 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -9,10 +9,10 @@ var removeDuplicates = function(nums) {
     
     let counter = 0
     
-    for (let i = 0 ; i< nums.length; i++) {
-        if (nums[i + 1] !== nums[i]) {
-            nums[counter] = nums[i]
-            counter ++
+    for (let i =0; i< nums.length;i++){
+        if (nums[i] !== nums[i+1]) {
+            nums[counter] = nums[i] 
+            counter++
         }
     }
     return counter
