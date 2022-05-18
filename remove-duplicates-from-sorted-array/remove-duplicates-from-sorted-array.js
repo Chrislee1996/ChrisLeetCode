dbc9842a7d7f94471d85cf7530b9bd5d047e8a48
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-18 19:34:43 -0400

    Time: 74 ms, Memory: 44.2 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,18 +3,41 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-         if (nums.length === 0 ) {
+     if (nums.length === 0 ) {
          return 0
      }
     
-    let arrayCounter = 0
+    let counter = 0
     
-    for (let i =0 ;i < nums.length ;i++) {
-        if (nums[i] !== nums[i + 1]) {
-            nums[arrayCounter] = nums[i]
-            arrayCounter++
+    for (let i = 0 ; i< nums.length; i++) {
+        if (nums[i + 1] !== nums[i]) {
+            nums[counter] = nums[i]
+            counter ++
         }
     }
-    return arrayCounter
-    
+    return counter
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let arrayCounter = 0
+    
+//     for (let i =0 ;i < nums.length ;i++) {
+//         if (nums[i] !== nums[i + 1]) {
+//             nums[arrayCounter] = nums[i]
+//             arrayCounter++
+//         }
+//     }
+//     return arrayCounter
