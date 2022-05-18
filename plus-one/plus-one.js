893fa7db124371af96e5ae16539ef140af95b046
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-18 18:53:03 -0400

    Time: 82 ms, Memory: 41.6 MB - LeetHub

--- plus-one/plus-one.js
+++ plus-one/plus-one.js
@@ -3,7 +3,8 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    for (let i = digits.length-1 ; i >= 0 ; i--) {
+    
+    for (let i = digits.length-1; i >= 0; i--) {
         if (digits[i] !== 9) {
             digits[i]++
             return digits
@@ -13,4 +14,29 @@ var plusOne = function(digits) {
     }
     digits.unshift(1)
     return digits
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // for (let i = digits.length-1 ; i >= 0 ; i--) {
+    //     if (digits[i] !== 9) {
+    //         digits[i]++
+    //         return digits
+    //     } else {
+    //         digits[i] = 0
+    //     }
+    // }
+    // digits.unshift(1)
+    // return digits
 };
