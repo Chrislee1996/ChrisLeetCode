4e9d17cc710c6e6e1b744d363353ab6074929adc
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-18 18:44:22 -0400

    Time: 77 ms, Memory: 42.1 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,35 +3,46 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-//           let buyPrice = prices[0]
-//           let max = 0 
 
-//       for (let i =1 ; i < prices.length; i++) {
-
-// //           i starts at 1 because we can't sell after we buy on the first day
-//           const sellPrice = prices[i]
-// //           when would be the best time to buy give that we were to sell on i? 
-//           const profit = sellPrice - buyPrice
-//           max = Math.max(max,profit)
-//           buyPrice = Math.min(buyPrice, prices[i])
-//       }
-//     return max
+    let profit = 0 
     
     
-//declare our profit to 0
-//iterate through the array of stock prices
-    //need to check if our current price is less than the price to the left so we can decide to sell or not
-    //if so, we know we can sell and declare our profit as the sold price
-    
-    let profit = 0
-    
-    for (let i =1 ; i < prices.length; i++) {
-        if (prices[i - 1] < prices[i]) {
-            //       [7,1,5,3,6,4]      
+    for (let i = 1 ; i < prices.length; i++) {
+        if (prices[i - 1 ] < prices[i]) {
             profit += prices[i] - prices[i - 1]
         }
     }
-    return profit
-    
+return profit
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let profit = 0
+    
+//     for (let i =1 ; i < prices.length; i++) {
+//         if (prices[i - 1] < prices[i]) {
+//             //       [7,1,5,3,6,4]      
+//             profit += prices[i] - prices[i - 1]
+//         }
+//     }
+//     return profit
