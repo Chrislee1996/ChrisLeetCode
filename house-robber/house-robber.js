cd19d57fd170b5d3bd765b010bf8233882482398
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-19 16:13:55 -0400

    Time: 101 ms, Memory: 41.6 MB - LeetHub

--- house-robber/house-robber.js
+++ house-robber/house-robber.js
@@ -3,7 +3,8 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if (nums.length === 0 ) {
+
+    if (nums.length === 0) {
         return 0
     }
     
@@ -11,14 +12,50 @@ var rob = function(nums) {
         return nums[0]
     }
     
-    let robbedTotal = []
+    let totalProfit = []
+     totalProfit[0] = nums[0]
+     totalProfit[1] = Math.max(nums[0] , nums[1])
     
-    robbedTotal[0] = nums[0]
-    robbedTotal[1] = Math.max(nums[0], nums[1])
-    
-    for (let i = 2; i < nums.length;i++) {
-        robbedTotal[i] = Math.max(robbedTotal[i - 1] , robbedTotal[i - 2 ] + nums[i])
+    for (let i = 2 ; i < nums.length ; i++) {
+        totalProfit[i] = Math.max(totalProfit[i - 1], nums[i] + totalProfit[i - 2])
     }
-    
-    return robbedTotal[robbedTotal.length - 1]
+    return totalProfit[totalProfit.length-1]
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     if (nums.length === 0 ) {
+//         return 0
+//     }
+    
+//     if (nums.length === 1) {
+//         return nums[0]
+//     }
+    
+//     let robbedTotal = []
+    
+//     robbedTotal[0] = nums[0]
+//     robbedTotal[1] = Math.max(nums[0], nums[1])
+    
+//     for (let i = 2; i < nums.length;i++) {
+//         robbedTotal[i] = Math.max(robbedTotal[i - 1] , robbedTotal[i - 2 ] + nums[i])
+//     }
+    
+//     return robbedTotal[robbedTotal.length - 1]
