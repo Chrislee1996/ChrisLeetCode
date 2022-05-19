709ab2bf257eb387c3792ca5662605f6ad9da9e1
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-19 12:59:49 -0400

    Time: 109 ms, Memory: 44.1 MB - LeetHub

--- reverse-integer/reverse-integer.js
+++ reverse-integer/reverse-integer.js
@@ -4,24 +4,24 @@
  */
 var reverse = function(x) {
 
-     let negative = x < 0
-     let reversed = 0
-     
-     if (negative) {
-         x *= -1
-     }
-
-    while (x > 0) {
-        reversed = (reversed * 10) + (x % 10)
-        x = Math.floor( x / 10 )
+    let negativeNumber = x < 0
+    let reversedNumber = 0
+    
+    if (negativeNumber ) {
+        x = x * -1
     }
     
+    while (x > 0) {
+        reversedNumber = (reversedNumber * 10) + (x % 10)
+        x = Math.floor( x  / 10)
+    }
     
-    if (reversed > (2 ** 31 -1)) {
+    if (reversedNumber > 2 ** 31 -1) {
         return 0
     }
     
-    return negative ? (reversed * -1) : reversed
+    return negativeNumber ? (reversedNumber * -1) : reversedNumber
+   
 };
 
 
@@ -44,22 +44,23 @@ var reverse = function(x) {
 
 
 
+//   let negative = x < 0
+//      let reversed = 0
+     
+//      if (negative) {
+//          x *= -1
+//      }
 
-    
-//     let negative = x < 0
-//     let reversed = 0
-    
-//     if (negative) {
-//         x *= -1
-//     }
-    
 //     while (x > 0) {
 //         reversed = (reversed * 10) + (x % 10)
-//         x = Math.floor( x / 10)
+//         x = Math.floor( x / 10 )
 //     }
     
-//     if (reversed > ( 2 ** 31 -1)) {
+    
+//     if (reversed > (2 ** 31 -1)) {
 //         return 0
 //     }
     
 //     return negative ? (reversed * -1) : reversed
+
+
