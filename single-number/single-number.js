471322dc730622217e076b9397505d2cc74084e2
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-18 21:46:45 -0400

    Time: 118 ms, Memory: 47.8 MB - LeetHub

--- single-number/single-number.js
+++ single-number/single-number.js
@@ -3,20 +3,35 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    //this will host our table of numbers ex. {1000 : 5} - number 1000 appears 5 times
-    const hash = {}
+    let hashmap = {}
     
-    for (const num of nums) {
-        //this creates a  count of our hash table - if a number appears, +1 will increase the number once - however, if a new number appears such as 1001 - the || 1 will now create a new count
-
-        hash[num] = hash[num] +1 || 1
+    for (let i=0;i<nums.length;i++ ) {
+        hashmap[nums[i]] = hashmap[nums[i]] +1 || 1
     }
     
-    //if our value in our ht only appears once(1) then we will return it
-    for (const value in hash) {
-        if (hash[value] === 1 ) {
-            console.log('here is our hash ', hash)
+    for (const value in hashmap) {
+        if (hashmap[value] === 1) {
             return value
         }
     }
 };
+
+
+
+
+//this will host our table of numbers ex. {1000 : 5} - number 1000 appears 5 times
+//     const hash = {}
+    
+//     for (const num of nums) {
+//         //this creates a  count of our hash table - if a number appears, +1 will increase the number once - however, if a new number appears such as 1001 - the || 1 will now create a new count
+
+//         hash[num] = hash[num] +1 || 1
+//     }
+    
+//     //if our value in our ht only appears once(1) then we will return it
+//     for (const value in hash) {
+//         if (hash[value] === 1 ) {
+//             console.log('here is our hash ', hash[value])
+//             return value
+//         }
+//     }
