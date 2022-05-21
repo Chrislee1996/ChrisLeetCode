3c705b9d9d6c1d02a60b5ddbc69b42e688b9ebff
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-21 09:26:47 -0400

    Time: 77 ms (82.10%), Space: 42.8 MB (45.66%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+
+    let hashmap = {}
+    
+    for (let i = 0 ; i < nums.length; i ++) {
+        let values = nums[i]
+        let pairSum = target - values
+        if (hashmap[pairSum] !== undefined) {
+            return [hashmap[pairSum], i]
+        } else {
+            hashmap[values] = i
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+};
+
+
+
+
+
+
+  //   let result = []
+  // for (let i =0; i< nums.length;i++){
+  //     for (let j = i+1; j < nums.length;j ++) {
+  //       if (nums[i] + nums[j] === target) {
+  //           return result = [i, j];
+  //         }
+  //     }
+  // }  
+    
+
+
+    
+//     let hashmap = {}
+    
+//     for (let i = 0 ; i < nums.length; i++) {
+//         let values = nums[i]
+//         let pairSum = target - values
+//         if (hashmap[pairSum] !== undefined) {
+//             return [hashmap[pairSum], i] 
+//         } else {
+//             hashmap[values] =i
+//         }
+//     }
