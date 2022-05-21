5cff9828ab9b0c0298f91995c1ca23a1a2369c8f
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-21 09:31:40 -0400

    Time: 80 ms (94.81%), Space: 51.2 MB (97.98%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,96 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0
+    let buyPrice = prices[0]
+    
+    for (let i = 1 ; i < prices.length; i++) {
+        if (prices[i] < buyPrice) {
+            buyPrice = prices[i]
+        } else if (prices[i] - buyPrice > profit){
+            profit = prices[i] - buyPrice
+        }
+    }
+    return profit
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let buyPrice = prices[0]
+//     let profit = 0
+    
+//     for (let i = 1 ;i < prices.length; i++) {
+//         if (prices[i] < buyPrice) {
+//             buyPrice = prices[i]
+//         } else if (prices[i] - buyPrice > profit) {
+//             profit =  prices[i] - buyPrice 
+//         }
+//     }
+//     return profit
