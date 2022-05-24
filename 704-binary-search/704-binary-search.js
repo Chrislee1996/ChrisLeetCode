66a690cf0ac0f3eaea0dfac3a00691ec0177c0f8
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-24 16:29:03 -0400

    Time: 72 ms (77.79%), Space: 45.5 MB (9.56%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,10 +4,29 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for (let i = 0; i < nums.length ; i++ ) {
-        if (nums[i] === target) {
-            return i
+    let leftPointer = 0
+    let rightPointer = nums.length-1
+    
+
+    while (leftPointer <= rightPointer) {
+        let middle = Math.floor((leftPointer + rightPointer) / 2)
+        if (target === nums[middle]) {
+            return middle
+        } else if (target < nums[middle]) {
+            rightPointer = middle -1
+        } else {
+            leftPointer = middle + 1
         }
     }
     return -1
 };
+
+
+
+
+  // for (let i = 0; i < nums.length ; i++ ) {
+  //       if (nums[i] === target) {
+  //           return i
+  //       }
+  //   }
+  //   return -1
