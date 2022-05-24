f65aca0e4f85cd580b7e47d30d92054ed161522f
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-24 17:08:03 -0400

    Time: 98 ms (30.42%), Space: 44.9 MB (81.64%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -8,16 +8,16 @@ var search = function(nums, target) {
     let leftPointer = 0
     let rightPointer = nums.length-1
     
-    while( leftPointer <= rightPointer) {
-        let middle = Math.floor((leftPointer + rightPointer) / 2)
-        if (nums[middle] === target) {
-            return middle
-        } else if (target < nums[middle]) {
-            rightPointer = middle - 1
-        } else {
-            leftPointer = middle + 1
-        }
-    }
+   while (leftPointer <= rightPointer) {
+       let middle = Math.floor((leftPointer + rightPointer)/2)
+       if (nums[middle] === target) {
+           return middle
+       } else if (target < nums[middle]) {
+           rightPointer = middle - 1
+       } else {
+           leftPointer = middle + 1
+       }
+   }
     return -1
 };
 
