b3971db7020d2c675dbbb4a331aa0e2c718953ee
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-26 14:32:41 -0400

    Time: 151 ms (6.50%), Space: 43.8 MB (61.68%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,21 +3,23 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let map = {}
     
-    for (let i = 0 ; i < nums.length ; i++) {
+    let hashmap = {}
+    
+    for (let i = 0 ; i < nums.length; i++) {
         let char = nums[i]
-        
-        if (!map[char]) {
-            map[char] = 1
+        if (!hashmap[char]) {
+            hashmap[char] = 1
         } else {
-            map[char]++
+            hashmap[char]++
         }
     }
     
-    for (let key in map) {
-        if (map[key] > Math.floor(nums.length/2)) {
-            return key
+    for (let values in hashmap) {
+        if (hashmap[values] > Math.floor(nums.length / 2)) {
+            return values
         }
     }
+    
+    
 };
