960d42a0b600ccf8dc7a44e04d9ec468589d3a21
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-27 18:28:15 -0400

    Time: 118 ms (61.02%), Space: 50.3 MB (57.14%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -5,21 +5,23 @@
  */
 var removeDuplicates = function(s, k) {
     let stack = []
-    let result = ''
     
-    for (let i = 0 ; i < s.length ; i++) {
+    for (let i = 0; i < s.length ; i++) {
         let char = s[i]
         let peek = stack[stack.length-1]
+        
         if (stack.length === 0 || peek[0] !== char) {
             stack.push([char,1])
         } else {
             peek[1]++
-            if (peek[1] === k ) stack.pop()
+            if (peek[1] === k) stack.pop()
         }
     }
     
-    for (let [characters, counter] of stack) {
-        result += characters.repeat(counter)
+    let result = ''
+    
+    for (let [char, counter] of stack) {
+        result += char.repeat(counter)
     }
     return result
 };
