ccfdd68213ac7091d5995470ac285a68979b7999
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-26 22:07:44 -0400

    Time: 109 ms (27.13%), Space: 43.4 MB (85.25%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,10 +3,9 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    
     let hashmap = {}
     
-    for (let i = 0 ; i < nums.length; i++) {
+    for (let i = 0 ; i < nums.length ; i++) {
         let char = nums[i]
         if (!hashmap[char]) {
             hashmap[char] = 1
@@ -16,10 +15,9 @@ var majorityElement = function(nums) {
     }
     
     for (let values in hashmap) {
-        if (hashmap[values] > Math.floor(nums.length / 2)) {
+        if(hashmap[values] > Math.floor(nums.length) / 2) {
             return values
         }
     }
-    
     
 };
