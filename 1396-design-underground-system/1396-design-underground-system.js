3d53fda098f7e18b3e422a00b4492382f1b98c1a
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-05-31 18:37:17 -0400

    Time: 301 ms (54.72%), Space: 62.3 MB (16.96%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.js
+++ 1396-design-underground-system/1396-design-underground-system.js
@@ -1,22 +1,65 @@
-class UndergroundSystem {
-    constructor() {
-        this.checkins = new Map()
-        this.routes = new Map()
-    }
-    checkIn(id, stationName, t) {
-        this.checkins.set(id, [stationName, t])
-    };
-    checkOut(id, stationName, t) {
-        let [stn, start] = this.checkins.get(id),
-            route = stn + "," + stationName
-        this.checkins.delete(id)
-        if (!this.routes.has(route))
-            this.routes.set(route, new Uint32Array(2))
-        let trip = this.routes.get(route)
-        trip[0]++, trip[1] += t - start
-    };
-    getAverageTime(startStation, endStation) {
-        let [count, sum] = this.routes.get(startStation + "," + endStation)
-        return sum / count
-    };
+var UndergroundSystem = function() {
+    this.completedTrips = {};
+    this.inProgressTrips = {};
 };
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+    this.inProgressTrips[id] = { startStation: stationName, startTime: t };
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
+    let { startStation, startTime } = this.inProgressTrips[id];
+    
+    if(this.completedTrips.hasOwnProperty(startStation)) {
+        let parent = this.completedTrips[startStation];
+        
+        if(!parent.hasOwnProperty(stationName)) {
+            parent[stationName] = [];
+        }
+        
+        let data = this.completedTrips[startStation][stationName];
+        data.push(t - startTime);
+    } else {
+        this.completedTrips[startStation] = { [stationName]: [t - startTime] };
+    }
+    
+    delete this.inProgressTrips[id];
+};
+
+/** 
+ * @param {string} startStation 
+ * @param {string} endStation
+ * @return {number}
+ */
+UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
+    let data = this.completedTrips[startStation][endStation];
+    let len = data.length;
+    
+    let sum = 0;
+    
+    for(let i = 0; i < data.length; i++) {
+        sum += data[i];
+    }
+    
+    return sum / len;
+};
+
+/** 
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
