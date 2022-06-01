af74df2a387042154e3dd339ee59c2db9ce608ef
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-01 11:16:44 -0400

    Time: 90 ms (28.69%), Space: 41.4 MB (97.68%) - LeetHub

--- 394-decode-string/394-decode-string.js
+++ 394-decode-string/394-decode-string.js
@@ -3,26 +3,27 @@
  * @return {string}
  */
 var decodeString = function(s) {
+    let multiplyStack = []
     let result = ''
     let tempString = ''
-    let multiplyStack = []
     let repeatStr = []
     
     for (let i = 0 ; i < s.length; i++) {
         let char = s[i]
-        
         if (!isNaN(char)) {
             tempString = `${tempString}${char}`
+            
+            
         } else if (char === '[') {
             multiplyStack.push(tempString)
-            tempString = ''
+            tempString=''
             
             repeatStr.push(result)
             result = ''
             
         } else if (char === ']') {
             
-            result = repeatStr.pop() + result.repeat(multiplyStack.pop())
+             result = repeatStr.pop() + result.repeat(multiplyStack.pop())
             
         } else {
             result += char
@@ -30,6 +31,3 @@ var decodeString = function(s) {
     }
     return result
 };
-
-
-// "3[a]2[bc]"
