2527306770495dfe651f226f65373281fc7d8930
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-01 14:08:45 -0400

    Time: 99 ms (29.54%), Space: 45.3 MB (32.92%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,97 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
+     let pointerOne = 0
+    let pointerTwo = nums.length-1
     
-    let leftPointer = 0
-    let rightPointer = nums.length-1
-    
-    while (leftPointer <= rightPointer) {
-        let middle = Math.floor((leftPointer + rightPointer) / 2 )
-        if (nums[middle] === target){
+    while(pointerOne <= pointerTwo) {
+        let middle = Math.floor((pointerOne + pointerTwo) / 2)
+        if (nums[middle] === target) {
             return middle
         } else if (target < nums[middle]) {
-            rightPointer = middle - 1
+            pointerTwo = middle - 1
         } else {
-            leftPointer = middle + 1
+            pointerOne = middle + 1
         }
     }
     return -1
 };
-
-
-
-
-
-  // for (let i = 0; i < nums.length ; i++ ) {
-  //       if (nums[i] === target) {
-  //           return i
-  //       }
-  //   }
-  //   return -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     let leftPointer = 0
-//     let rightPointer = nums.length-1
-    
-
-//     while (leftPointer <= rightPointer) {
-//         let middle = Math.floor((leftPointer + rightPointer) / 2)
-//         if (target === nums[middle]) {
-//             return middle
-//         } else if (target < nums[middle]) {
-//             rightPointer = middle -1
-//         } else {
-//             leftPointer = middle + 1
-
-//         }
-//     }
-//     return -1
