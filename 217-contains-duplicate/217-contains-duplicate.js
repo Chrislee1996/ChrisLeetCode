7b91b0ce32350f924d295e5413c4e155c8a02182
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-02 17:47:47 -0400

    Time: 240 ms (17.89%), Space: 55.1 MB (5.08%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,13 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
     let hashmap = {}
     
-    for (let i = 0 ; i < nums.length ; i++) {
-        let values = nums[i]
+    for (let values of nums) {
         if (!hashmap[values]) {
-            hashmap[values] = 1 
+            hashmap[values] = 1
         } else {
             hashmap[values]++
         }
@@ -18,34 +16,7 @@ var containsDuplicate = function(nums) {
     for (let values in hashmap) {
         if (hashmap[values] > 1) {
             return true
-        }
+        } 
     }
     return false
 };
-
-
-
-
-
-//     let numberSet = new Set()
-    
-//     for (let i =0 ; i< nums.length ; i++) {
-//         if (!numberSet.has(nums[i])) {
-//             numberSet.add(nums[i])
-//         } else {
-//             return true
-//         }
-//     }
-//     return false
-
-//     let numbers = new Set()
-    
-//     for (let num of nums) {
-//         if (!numbers.has(num)) {
-//             numbers.add(num)
-//         } else {
-//             return true
-//         }
-//     }
-    
-//     return false
