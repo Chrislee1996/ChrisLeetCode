78ed532fba3ebe3627aba46a03c6fe3473d7ec51
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-04 13:32:18 -0400

    Time: 112 ms (61.86%), Space: 49.7 MB (23.12%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let leftPointer = 0
+    let rightPointer = s.length-1
+    while (leftPointer < rightPointer) {
+        let temp = s[leftPointer]
+        s[leftPointer] = s[rightPointer]
+        s[rightPointer] = temp
+        leftPointer ++
+        rightPointer--
+    }
+    return s
+};
