026e3b0d6629ec2437dc10b5b71e5086bb91dad8
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-09 10:35:39 -0400

    Time: 121 ms (11.54%), Space: 43.1 MB (54.06%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,17 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-
-        
     let leftPointer = 0
     let rightPointer = numbers.length-1
-    
     let result = []
     
     while (leftPointer < rightPointer) {
         if (numbers[leftPointer] + numbers[rightPointer] === target) {
-            result.push(leftPointer + 1) 
-            result.push(rightPointer + 1)
+            result.push(leftPointer + 1)
+            result.push(rightPointer +1)
             break
         } else if (numbers[leftPointer] + numbers[rightPointer] > target) {
             rightPointer--
@@ -23,13 +20,4 @@ var twoSum = function(numbers, target) {
         }
     }
     return result
-    
-    
 };
-
-
-
-//2 pointers
-//1 pointer at index 0
-//1 pointer at index 1
-//give our pairsum to equal numbers[i] - target
