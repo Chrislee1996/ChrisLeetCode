dd05ef0fa36d27eac786e672978bf373ee384f66
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-13 17:11:16 -0400

    Time: 85 ms (39.10%), Space: 41.9 MB (70.29%) - LeetHub

---
+++ 198-house-robber/198-house-robber.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums.length === 0 ) {
+        return 0
+    }
+    
+    if (nums.length === 1) {
+        return nums[0]
+    }
+    
+    let robbedTotal = []
+    robbedTotal[0] = nums[0]
+    robbedTotal[1] = Math.max(nums[0], nums[1])
+    
+    for (let i = 2; i < nums.length; i++) {
+        robbedTotal[i] = Math.max(robbedTotal[i-1] , robbedTotal[i-2] + nums[i])
+    }
+    
+    return robbedTotal[robbedTotal.length-1]
+};
