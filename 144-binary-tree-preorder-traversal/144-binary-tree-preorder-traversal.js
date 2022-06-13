abb6f5d568b738beb80de6d528d3e31d2c47b8d1
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-13 19:45:36 -0400

    Time: 74 ms (60.41%), Space: 42.6 MB (6.26%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -18,12 +18,12 @@ var preorderTraversal = function(root) {
     let stack = [root]
     let result = []
     
-    while (stack.length) {
-        let node = stack.pop()
+    while (stack.length > 0) {
+        const node = stack.pop()
         result.push(node.val)
         
-        if (node.right) stack.push(node.right)
-        if (node.left) stack.push(node.left)
+        if (node.right !==null) stack.push(node.right)
+        if (node.left !== null) stack.push(node.left)
     }
     return result
 };
