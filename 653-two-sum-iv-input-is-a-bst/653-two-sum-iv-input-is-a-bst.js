296acb9391d2e0d2765606db95bd968b5fa2d505
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-14 16:13:47 -0400

    Time: 162 ms (27.54%), Space: 51.8 MB (52.48%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
@@ -21,9 +21,11 @@ var findTarget = function(root, k) {
     } 
       const set = new Set();
       const stack = [root];
-      while (stack.length) {
+      while (stack.length > 0) {
         const node = stack.pop();
-        if (set.has(k - node.val)) return true;
+        if (set.has(k - node.val)) {
+            return true;
+        }
         set.add(node.val);
         if (node.right) stack.push(node.right);
         if (node.left) stack.push(node.left);
