b925d5c7de38104da604f371ce17ad8eb5ef388a
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-14 15:22:31 -0400

    Time: 93 ms (26.30%), Space: 41.7 MB (97.64%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -12,28 +12,28 @@
  */
 var inorderTraversal = function(root) {
     const stack = [];
-//     const res = [];
-//     let pointerOne = root
+    const res = [];
+    let pointerOne = root
 
-//   while (pointerOne !== null || stack.length > 0) {
-//     if (pointerOne) {
-//       stack.push(pointerOne);
-//       pointerOne = pointerOne.left;
-//     } else {
-//       pointerOne = stack.pop();
-//       res.push(pointerOne.val);
-//       pointerOne = pointerOne.right;
-//     }
-//   }
-
-//   return res;
-    const helperFunc = (node) =>{
-        if (!node) return;
-        helperFunc(node.left)
-        stack.push(node.val)
-        helperFunc(node.right)
+  while (pointerOne !== null || stack.length > 0) {
+    if (pointerOne) {
+      stack.push(pointerOne);
+      pointerOne = pointerOne.left;
+    } else {
+      pointerOne = stack.pop();
+      res.push(pointerOne.val);
+      pointerOne = pointerOne.right;
     }
+  }
 
-    helperFunc(root)
-    return stack
+  return res;
+//     const helperFunc = (node) =>{
+//         if (!node) return;
+//         helperFunc(node.left)
+//         stack.push(node.val)
+//         helperFunc(node.right)
+//     }
+
+//     helperFunc(root)
+//     return stack
 };
