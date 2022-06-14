5ec84b554270d8cf497820ccf6a66554409c8c43
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-14 18:30:18 -0400

    Time: 78 ms (53.36%), Space: 41.9 MB (95.00%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -14,9 +14,10 @@
 var isSameTree = function(p, q) {
 
     
-    if (!p && !q) return true;
-    // not the same if either of the nodes is null or the values are different
-    if ((!p && q) || (p && !q) || p.val !== q.val) return false;
+    // if (!p && !q) return true;
+    if (p === null && q === null) return true
+    if ((!p && q) || (p && !q)) return false;
+    if (p.val !== q.val) return false
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
     
