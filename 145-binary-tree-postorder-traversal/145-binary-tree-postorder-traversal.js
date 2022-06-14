69f259808b396d7a3f35626552bddea74f6b3341
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-14 15:37:25 -0400

    Time: 99 ms (19.14%), Space: 41.6 MB (96.94%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
@@ -11,27 +11,44 @@
  * @return {number[]}
  */
 var postorderTraversal = function(root) {
-    if (!root) {
-        return []
-    }
-    let stack = []
-    let result = [];
-    let pointerOne = root
-    stack.push(pointerOne)
+    // if (!root) {
+    //     return []
+    // }
+    // let stack = []
+//     let result = [];
+//     let pointerOne = root
+//     stack.push(pointerOne)
 
-    while(stack.length) {
-        let node = stack[stack.length-1]
-        if (node.left) {
-            stack.push(node.left)
-            node.left = null
-        } else if (node.right) {
-            stack.push(node.right)
-            node.right = null
-        }
-        else result.push(stack.pop().val)
+//     while(stack.length) {
+//         let node = stack[stack.length-1]
+//         if (node.left) {
+//             stack.push(node.left)
+//             node.left = null
+//         } else if (node.right) {
+//             stack.push(node.right)
+//             node.right = null
+//         }
+//         else result.push(stack.pop().val)
+//     }
+//     return result
+    
+    let result = []
+    
+    const helperFunc = (node) => {
+        if (!node) return;
+        helperFunc(node.left)
+        helperFunc(node.right)
+        result.push(node.val)
     }
+    
+    helperFunc(root)
     return result
     
+};
+
+
+
+
     // stack.push(root);
     // while (stack.length) {
     //     let node = stack[stack.length-1];
@@ -46,4 +63,3 @@ var postorderTraversal = function(root) {
     //     else res.push(stack.pop().val);
     // }
     // return res;
-};
