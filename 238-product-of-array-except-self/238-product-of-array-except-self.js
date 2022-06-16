0ab3b02d00f26b7eaa1d34cce9097c0f668e3f58
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-16 17:04:11 -0400

    Time: 181 ms (24.29%), Space: 51.8 MB (98.71%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,26 +3,21 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-  let leftArr = [];
-  let leftMultiplication = 1;
-
-  for (let i=0; i < nums.length; i++) {
-    leftArr[i] = leftMultiplication;
-    leftMultiplication *=  nums[i];
-  }
+    let leftArray = []
+    let leftMultiplier = 1
     
-  let rightArr = [];
-  let rightMultiplication = 1;
-
-  for (let i=nums.length-1; i >= 0; i--) {
-    rightArr[i] = rightMultiplication;
-    rightMultiplication *= nums[i];
-    rightArr[i] *= leftArr[i]; //this additional step saves us from having another iteration. We can do the multiplication at the spot.
-  }
-  
-  return rightArr;
-
+    for (let i = 0 ; i < nums.length ; i++) {
+        leftArray[i] = leftMultiplier
+        leftMultiplier *= nums[i]
+    }
     
+    let rightArray = []
+    let rightMultiplier = 1
+    
+    for (let i = nums.length-1 ; i>=0;i--) {
+        rightArray[i] = rightMultiplier
+        rightMultiplier *= nums[i]
+        rightArray[i] *= leftArray[i]
+    }
+    return rightArray
 };
-    
-
