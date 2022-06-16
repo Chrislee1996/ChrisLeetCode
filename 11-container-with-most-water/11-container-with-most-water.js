2325620a20487c3833f33adc97b9840d9e7ced6b
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-16 15:01:16 -0400

    Time: 120 ms (32.87%), Space: 50 MB (15.48%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var maxArea = function(height) {
-  let result = 0
-  let leftPointer = 0
-  let rightPointer = height.length-1
-  
-  while (leftPointer < rightPointer) {
-      let area = (rightPointer - leftPointer) * Math.min(height[leftPointer], height[rightPointer])
-      result = Math.max(result, area)
-      
-      if (height[leftPointer] < height[rightPointer]) {
-          leftPointer++ 
-      } else {
-          rightPointer--
-      }
-  }
+    let result = 0
+    let leftPointer = 0 
+    let rightPointer = height.length-1
+    
+    while(leftPointer < rightPointer) {
+        let area = (rightPointer - leftPointer) * Math.min(height[leftPointer] , height[rightPointer])
+        result = Math.max(area, result)
+        
+        if (height[leftPointer] < height[rightPointer]) {
+            leftPointer++
+        } else {
+            rightPointer--
+        }
+    }
     return result
 };
