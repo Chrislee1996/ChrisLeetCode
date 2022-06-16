2a2bdb5106a5ef1d72c0a6a553f987ab9ec0b4fe
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-16 16:44:56 -0400

    Time: 215 ms (13.29%), Space: 52.3 MB (96.29%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,24 +3,25 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let left = []
-    let right = []
-    let result = []
+  let leftArr = [];
+  let leftMultiplication = 1;
 
-    left[0] = 1;
-    for(let i = 1; i < nums.length; i++) {
-        left[i] = left[i - 1] * nums[i - 1];
-    }
+  for (let i=0; i < nums.length; i++) {
+    leftArr[i] = leftMultiplication;
+    leftMultiplication *=  nums[i];
+  }
+    
+  let rightArr = [];
+  let rightMultiplication = 1;
 
-    right[nums.length - 1] = 1;
-    for(let i = nums.length - 2; i >= 0; i--) {
-        right[i] = right[i + 1] * nums[i + 1];
-    }
+  for (let i=nums.length-1; i >= 0; i--) {
+    rightArr[i] = rightMultiplication;
+    rightMultiplication *= nums[i];
+    rightArr[i] *= leftArr[i]; //this additional step saves us from having another iteration. We can do the multiplication at the spot.
+  }
+  
+  return rightArr;
 
-    for(let i=0; i<nums.length; i++) {
-        result[i] = left[i] * right[i];
-    }
-    return result;
     
 };
     
