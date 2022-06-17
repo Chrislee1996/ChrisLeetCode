8faaef3cff2785c2e624dacc5e36af23191a9d74
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-16 22:25:56 -0400

    Time: 104 ms (79.73%), Space: 47.4 MB (8.93%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,17 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let slowPointer = 0
-    let fastPointer = 0
-    
-    for (let fastPointer = 0 ; fastPointer < nums.length; fastPointer++) {
-        if (nums[fastPointer] !== 0) {
-            let temp = nums[slowPointer]
-            nums[slowPointer] = nums[fastPointer]
-            nums[fastPointer] = temp
-            
-            slowPointer ++
-            
+    let pointerOne = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[pointerOne], nums[i]] = [nums[i], nums[pointerOne]];
+            pointerOne++;
         }
     }
+    return nums;  
 };
