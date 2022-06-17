1ad4a29ff74483cd4e2e7216f0b8d2c8f1488e4d
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-16 20:55:29 -0400

    Time: 675 ms (16.21%), Space: 57.9 MB (32.34%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -4,24 +4,20 @@
  */
 var longestConsecutive = function(nums) {
     let set = new Set()
-    
-
-    
-    for (let i = 0 ; i < nums.length;i++) {
-        let number = nums[i]
-        set.add(number)
-    }
-    
     let longestStreak = 0
     
-    for (let num of set) {
-        if (!set.has(num-1)) {
-            let currentNum = num
+    for (let num of nums){
+        set.add(num)
+    }
+    
+    for (let num of nums) {
+        if (!set.has(num - 1)) {
             let currentStreak = 1
+            let currentNumber = num
             
-            while (set.has(currentNum + 1)) {
+            while (set.has(currentNumber + 1)) {
                 currentStreak +=1
-                currentNum +=1
+                currentNumber +=1
             }
             longestStreak = Math.max(longestStreak, currentStreak)
         }
