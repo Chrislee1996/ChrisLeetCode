3a01b1981f5f88de1dfc2cb1a9a2991cd1037f58
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-24 13:40:35 -0400

    Time: 102 ms (21.09%), Space: 41.9 MB (80.26%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let leftPointer = 0 
+    let rightPointer = nums.length-1
+    
+    while (leftPointer < rightPointer) {
+        let middle = Math.floor((leftPointer + rightPointer)/ 2)
+        if (nums[middle] > nums[rightPointer]) {
+            leftPointer = middle + 1
+        } else {
+            rightPointer = middle
+        }
+    }
+    return nums[leftPointer]
+};
