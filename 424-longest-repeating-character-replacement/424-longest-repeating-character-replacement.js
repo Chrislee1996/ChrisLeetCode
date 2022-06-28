87751245d338cfaf86663e6cb5deef8289dd7785
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-28 11:10:41 -0400

    Time: 165 ms (36.22%), Space: 42.4 MB (81.31%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -6,53 +6,27 @@
 var characterReplacement = function(s, k) {
     let leftPointer = 0
     let rightPointer = 0
-    let maxCharCount = 0
-    const hashmap = {}
+    let maxChar = 0
+    let hashmap = {}
     
-    while (rightPointer < s.length) {
-        const char = s[rightPointer]
-        
-        if (!hashmap[char]) {
-            hashmap[char] = 1
-        } else {
-            hashmap[char]++
-        }
-        
-        if (hashmap[char] > maxCharCount) {
-            maxCharCount = hashmap[char]
-        }
-        
-        if (rightPointer - leftPointer + 1 - maxCharCount > k) {
-            hashmap[s[leftPointer]]--
-            leftPointer++
-        } 
-        rightPointer++
-    }
+   while (rightPointer < s.length) {
+       let char = s[rightPointer]
+       
+       if (!hashmap[char]) {
+           hashmap[char] = 1
+       } else {
+           hashmap[char]++
+       }
+       
+       if (hashmap[char] > maxChar) {
+           maxChar = hashmap[char]
+       }
+       
+       if (rightPointer - leftPointer + 1 - maxChar > k) {
+           hashmap[s[leftPointer]]--
+           leftPointer++
+       }
+       rightPointer++
+   }
     return rightPointer - leftPointer
 };
-
-
-
-
-
-//   let left = 0;
-//   let right = 0;
-//   let maxCharCount = 0;
-//   const visited = {};
-
-//   while (right < s.length) {
-//     const char = s[right];
-//     visited[char] = visited[char] ? visited[char] + 1 : 1;
-
-//     if (visited[char] > maxCharCount) maxCharCount = visited[char];
-
-//     if (right - left + 1 - maxCharCount > k) {
-//       visited[s[left]]--;
-//       left++;
-//     }
-
-//     right++;
-//   }
-
-//   return right - left;
-// };
