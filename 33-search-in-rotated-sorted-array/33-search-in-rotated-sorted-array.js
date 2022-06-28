b1b5246a83a36fc40a60977d5d4f7104c9a11db8
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-28 11:31:49 -0400

    Time: 94 ms (36.98%), Space: 41.6 MB (95.60%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -7,72 +7,25 @@ var search = function(nums, target) {
     let leftPointer = 0
     let rightPointer = nums.length-1
     
-    while (leftPointer <= rightPointer) {
-        let mid = Math.floor((leftPointer + rightPointer) / 2)
-        if (nums[mid] === target) {
-            return mid
+    while(leftPointer <= rightPointer) {
+        let middle = Math.floor((leftPointer + rightPointer)/2)
+        if (nums[middle] === target) {
+            return middle
         }
         
-        if (nums[leftPointer] <= nums[mid]) {
-            if (nums[leftPointer] <= target && target <= nums[mid]) {
-                rightPointer = mid - 1
+        if (nums[leftPointer] <= nums[middle]) {
+            if (nums[leftPointer] <= target && nums[middle] >= target) {
+                rightPointer = middle - 1
             } else {
-                leftPointer = mid + 1
+                leftPointer = middle + 1
             }
         } else {
-            if (nums[mid] <= target && target <= nums[rightPointer]) {
-                leftPointer = mid + 1
+            if (nums[middle] <= target && target <= nums[rightPointer]) {
+                leftPointer = middle + 1
             } else {
-                rightPointer = mid - 1
+                rightPointer = middle - 1
             }
         }
     }
     return -1
 };
-
-
-
-
-
-
-
-// let left = 0;
-//   let right = nums.length - 1;
-    
-//   while (left <= right) {
-//     let mid = Math.floor((left + right) / 2);
-    
-//     if (nums[mid] === target) {
-//       return mid;
-//     }
-    
-//     // When dividing the roated array into two halves, one must be sorted.
-    
-//     // Check if the left side is sorted
-//     if (nums[left] <= nums[mid]) {
-//       if (nums[left] <= target && target <= nums[mid]) {
-//         // target is in the left
-//         right = mid - 1;
-        
-//       } else {
-//         // target is in the right
-//         left = mid + 1;
-//       }
-//     } 
-    
-//     // Otherwise, the right side is sorted
-//     else {
-//       if (nums[mid] <= target && target <= nums[right]) {
-//         // target is in the right
-//         left = mid + 1;
-
-//       } else {
-//         // target is in the left
-//         right = mid - 1;
-//       }
-//     }
-    
-    
-//   }
-    
-//   return -1;
