39f5f098003142354899bd10d46f2486469e95fe
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-29 16:47:03 -0400

    Time: 64 ms (89.94%), Space: 42.2 MB (46.38%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,20 +4,17 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let pointerOne = 0
-    let pointerTwo = nums.length-1 
-    
-    while(pointerOne <=pointerTwo) {
-        let middle = Math.floor((pointerOne + pointerTwo)/2)
-            if (nums[middle] === target) {
-                return middle
-            } else if (nums[middle] < target) {
-                pointerOne = middle + 1
-            } else {
-                pointerTwo = middle -1
-            }
+    let leftPointer = 0 
+    let rightPointer = nums.length-1
+    while(leftPointer <= rightPointer) {
+        let middle = Math.floor((leftPointer + rightPointer) / 2)
+        if (nums[middle] === target) {
+            return middle
+        } else if (nums[middle] < target ) {
+            leftPointer = middle + 1
+        } else {
+            rightPointer = middle -1 
         }
-    return pointerOne
- 
-    
+    }
+    return leftPointer
 };
