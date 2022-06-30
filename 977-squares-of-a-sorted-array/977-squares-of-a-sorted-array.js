8dc6fd13563d893be8548a15e7a7f449d8a03c8b
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-30 10:19:07 -0400

    Time: 104 ms (95.04%), Space: 48.2 MB (90.98%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -4,8 +4,9 @@
  */
 var sortedSquares = function(nums) {
     for (let i = 0 ; i < nums.length ; i++) {
-        nums[i] *= nums[i] 
+         nums[i] *= nums[i]
     }
-    return nums.sort((a,b) => a-b);
-    console.log(nums)
-}; 
+    return nums.sort((a,b) => 
+        a-b
+    )
+};
