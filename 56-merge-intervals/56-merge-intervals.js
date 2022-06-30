d68c80bdd4e06916dbef52b75111285a30eefde2
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-06-30 17:59:49 -0400

    Time: 155 ms (55.74%), Space: 48.1 MB (95.40%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -3,37 +3,21 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if(intervals.length < 2){
-        return intervals; 
-    } 
-    intervals.sort((a,b) => a[0] - b[0])
+
+    
+    intervals.sort((a,b) => a[0]-b[0])
     let result = [intervals[0]]
     
     for (let interval of intervals) {
         let endOne = result[result.length-1][1]
-        let startTwo = interval[0]
         let endTwo = interval[1]
+        let startTwo = interval[0]
         
         if (endOne >= startTwo) {
-            result[result.length-1][1] = Math.max(endOne, endTwo)
+            result[result.length-1][1] = Math.max(endTwo, endOne)
         } else {
             result.push(interval)
         }
     }
     return result
 };
-
-
-// var merge = function(intervals) {
-//   intervals.sort((a, b) => a[0] - b[0])
-//   const res = [intervals[0]]
-//   for (let curr of intervals) {
-//     prev = res[res.length - 1]
-//     if (prev[1] >= curr[0]) {
-//       prev[1] = Math.max(curr[1], prev[1])
-//     } else {
-//       res.push(curr)
-//     }
-//   }
-//   return res
-// };
