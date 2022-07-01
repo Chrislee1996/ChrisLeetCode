1c1237a8cfdb9e2fbecff7947f746b7b8703c6ca
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-01 16:08:44 -0400

    Time: 100 ms (26.15%), Space: 42.4 MB (34.93%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -5,21 +5,35 @@
  */
 var searchMatrix = function(matrix, target) {
 
-    for(let i = 0 ; i < matrix.length;i++) {
-            let startPointer = 0
-    let endPointer = matrix[0].length-1
-        while(startPointer <= endPointer) {
-            let middle = Math.floor((startPointer + endPointer)/ 2) 
-            if (matrix[i][middle] === target) {
-                return true
-            } else if (matrix[i][middle] > target) {
-                endPointer = middle - 1
-            } else {
-                startPointer = middle + 1
-            }
+    // for(let i = 0 ; i < matrix.length;i++) {
+    // let startPointer = 0
+    // let endPointer = matrix[0].length-1
+    //     while(startPointer <= endPointer) {
+    //         let middle = Math.floor((startPointer + endPointer)/ 2) 
+    //         if (matrix[i][middle] === target) {
+    //             return true
+    //         } else if (matrix[i][middle] > target) {
+    //             endPointer = middle - 1
+    //         } else {
+    //             startPointer = middle + 1
+    //         }
+    //     }
+    // }
+    // return false
+    
+        let row = 0 
+    let col = matrix[0].length-1
+    
+    while (row < matrix.length && col >= 0) {
+        if (matrix[row][col] === target) {
+            return true
+        } else if (matrix[row][col] > target) {
+            col--
+        } else {
+            row ++
         }
     }
-            return false
+    return false
 };
 
 
