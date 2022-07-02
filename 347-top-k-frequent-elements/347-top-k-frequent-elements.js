7cbcfc0cf8d650e00176636c11b330ba1280e7f2
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-02 12:35:04 -0400

    Time: 91 ms (84.26%), Space: 46.7 MB (39.03%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = new Map()
+    const bucket = []
+    const result = []
+    
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1)
+    }
+    
+    for (let [num, freq] of map) {
+        bucket[freq] = (bucket[freq] || new Set()).add(num)
+    }
+    
+    for(let i = bucket.length-1; i>=0 ; i--) {
+        if (bucket[i]) {
+            result.push(...bucket[i])
+        }
+        if (result.length === k) {
+            break
+        }
+    }
+    return result
+    
+};
+
+
+
+
+//     const freqMap = new Map();
+//     const bucket = [];
+//     const result = [];
+    
+//     for(let num of nums) {
+//         freqMap.set(num, (freqMap.get(num) || 0) + 1);
+//     }
+    
+//     for(let [num, freq] of freqMap) {
+//         bucket[freq] = (bucket[freq] || new Set()).add(num);
+//     }
+    
+//     for(let i = bucket.length-1; i >= 0; i--) {
+//         if(bucket[i]) result.push(...bucket[i]);
+//         if(result.length === k) break;
+//     }
+//     return result;
+// };
