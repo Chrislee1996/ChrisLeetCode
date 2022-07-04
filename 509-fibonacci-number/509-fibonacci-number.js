03834e4d6374e865a41cef47c2e9fe068dae24c8
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-04 17:58:30 -0400

    Time: 82 ms (70.81%), Space: 41.9 MB (59.42%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,8 +3,21 @@
  * @return {number}
  */
 var fib = function(n) {
-    if (n===0) return 0
-    if (n===1) return 1
+//     if (n===0) return 0
+//     if (n===1) return 1
     
-    return fib(n-1) + fib(n-2)
+//     return fib(n-1) + fib(n-2)
+    
+        if (n == 0 || n == 1) {
+        return n
+    }
+    
+    let arr = [0, 1]
+    
+    // n-1 due to if it wants to find n=5, it would need to be based off fib(4)+fib(3)
+    for (let i=2; i<=n; i++) {
+        arr.push(arr[i-1] + arr[i-2])
+    }
+    
+    return arr[arr.length-1]
 };
