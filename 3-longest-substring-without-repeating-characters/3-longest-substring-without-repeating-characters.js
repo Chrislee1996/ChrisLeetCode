58f42afdc073d92cab3f18057fe02b4b0b25adab
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-04 12:13:08 -0400

    Time: 152 ms (49.18%), Space: 47.4 MB (63.67%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -6,20 +6,33 @@ var lengthOfLongestSubstring = function(s) {
     let set = new Set()
     let slowPointer = 0
     let fastPointer = 0
-    
     let longestSubString = 0
-    while (fastPointer < s.length) {
+    
+    while(fastPointer < s.length) {
         if (!set.has(s.charAt(fastPointer))) {
             set.add(s.charAt(fastPointer))
             fastPointer++
-            longestSubString = Math.max(longestSubString , set.size)
+            longestSubString = Math.max(longestSubString, set.size)
         } else {
             set.delete(s.charAt(slowPointer))
             slowPointer++
         }
     }
-    
-    
-    
     return longestSubString
 };
+
+
+//a b c a b c b b
+//        S
+//              F
+
+
+
+
+//set: {
+    //a
+    //b
+    //c
+//}
+
+//longestSubString = 3 
