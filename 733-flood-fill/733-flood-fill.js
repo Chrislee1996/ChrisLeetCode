bfb52c0402497cce950ebf22c5c57f5cf04465b8
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-06 11:59:22 -0400

    Time: 101 ms (59.58%), Space: 44.8 MB (20.34%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -2,41 +2,26 @@
  * @param {number[][]} image
  * @param {number} sr
  * @param {number} sc
- * @param {number} newColor
+ * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, newColor) {
+var floodFill = function(image, sr, sc, color) {
     let currentColor = image[sr][sc]
-    if (newColor === currentColor) {
+    if (currentColor === color) {
         return image
-    }
+    }  
     
     function callDFS(image, row, col) {
-        if (row >= image.length || row < 0 || col >= image[0].length || col < 0 || image[row][col] !== currentColor) {
+        if (row >= image.length || row < 0 || col >= image[0].length || col < 0 || image[row][col] !== currentColor ) {
             return
         }
-        image[row][col] = newColor
-        callDFS(image, row + 1, col)
-        callDFS(image, row - 1, col)
-        callDFS(image, row, col +1)
-        callDFS(image, row, col -1)
+        image[row][col] = color
+        callDFS(image, row +1 , col)
+        callDFS(image, row -1, col)
+        callDFS(image, row , col +1)
+        callDFS(image, row , col -1)
         return image
+
     }
     return callDFS(image,sr,sc)
 };
-
-
-
-//     const currColor = image[sr][sc];
-//     if(newColor === currColor) return image;
-    
-//     function callDFS(img, row, col) {
-//         if(row >= img.length || row < 0 || col >= img[0].length || col < 0 || img[row][col] !== currColor) return;
-//         img[row][col] = newColor;
-// 		callDFS(img, row-1, col);  //up
-//         callDFS(img, row+1, col);  //down
-//         callDFS(img, row, col+1);  //right 
-//         callDFS(img, row, col-1);  //left
-//         return img;
-//     }
-//     return callDFS(image, sr, sc);
