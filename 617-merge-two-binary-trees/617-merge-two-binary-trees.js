3af3bbee59806876f45344e603927c4bab731556
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-06 15:23:43 -0400

    Time: 110 ms (85.34%), Space: 50.1 MB (65.16%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.js
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.js
@@ -17,9 +17,29 @@ var mergeTrees = function(root1, root2) {
     }
     
     const root = new TreeNode(root1.val + root2.val)
-    const left = mergeTrees(root1.left , root2.left)
-    const right = mergeTrees(root1.right, root2.right)
-    root.left = left
-    root.right = right
+    root.left = mergeTrees(root1.left , root2.left)
+    root.right = mergeTrees(root1.right, root2.right)
     return root
+    
+    
 };
+
+
+   // goal is to merge t2 to t1
+    
+    // if one of the node missing, return the other
+//     if (t1 === null) {
+//         return t2;
+//     }
+//     if (t2 === null) {
+//         return t1;
+//     }
+//     // if both nodes exist, sum the values
+//     t1.val += t2.val;
+    
+//     // do the same thing for left and right branch
+//     t1.left = mergeTrees(t1.left, t2.left);
+//     t1.right = mergeTrees(t1.right, t2.right);
+    
+//     // return the merged t1
+//     return t1;
