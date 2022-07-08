44e6d24f556128f60ace6beafb306d2da9e09e62
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-08 15:45:23 -0400

    Time: 220 ms, Memory: 52.8 MB - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -3,58 +3,59 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    if (nums.length < 3) {
-        return []
-    }
     
-   nums.sort((a,b) => {
-        return a-b
-    }) 
+    //[-1,0,1,2,-1,-4]
+    //-4   -1   -1   0   1   2
+    //               i        L   R
     
+        //-1   -1   -1   0   2   2   3
+        // i    L            R
     let result = []
     
-    for (let i = 0 ; i<nums.length; i++) {
-        if (nums[i] > 0 ) {
+    if (nums.length < 3) {
+        return result
+    }
+    
+    nums.sort((a, b) => 
+         a-b
+    )
+    
+    for (let i = 0 ; i < nums.length ;i++) {
+        if (nums[i] > 0) {
             break
         }
         
-        if (nums[i] === nums[i-1]) {
-            continue
+        if (nums[i] === nums[i - 1]) {
+            continue 
         }
         
         let leftPointer = i + 1
         let rightPointer = nums.length-1
         
-        while(leftPointer < rightPointer) {
+        while (leftPointer < rightPointer) {
             let sum = nums[i] + nums[leftPointer] + nums[rightPointer]
             
             if (sum === 0 ) {
-                result.push([nums[i] , nums[leftPointer], nums[rightPointer]])
+                result.push([nums[i] , nums[leftPointer] , nums[rightPointer]])
+                leftPointer++
+                rightPointer--
+             
                 
-                leftPointer++
-                rightPointer--
-            
-            
-            while(leftPointer < rightPointer && nums[leftPointer] === nums[leftPointer - 1]) {
-                leftPointer++
-            }
-             while(leftPointer < rightPointer && nums[rightPointer] === nums[rightPointer + 1]) {
-                rightPointer--
-            }
-            
+                while (leftPointer < rightPointer && nums[rightPointer] === nums[leftPointer - 1]) {
+                    leftPointer++
+                }
+                
+                while (leftPointer < rightPointer && nums[rightPointer] === nums[rightPointer + 1]) {
+                    rightPointer--
+                }
+                
             } else if (sum < 0) {
                 leftPointer++
             } else {
                 rightPointer--
             }
         }
+        
     }
     return result
 };
-    
-    
-    
-    //-4    -1   -1   0   1   2
-    //i      L                R
-    
-    //-4 + -1 + 2
