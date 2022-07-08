a6677ae418363875cb24c716a1be686b5f9a2cf0
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-08 13:07:48 -0400

    Time: 241 ms, Memory: 53.5 MB - LeetHub

---
+++ group-anagrams/group-anagrams.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let hashmap = {}
+    
+    for (let i = 0 ; i < strs.length ; i ++) {
+        let sortedArray = strs[i].split('').sort().join('')
+        
+        if (!hashmap[sortedArray]) {
+            hashmap[sortedArray] = [strs[i]]
+        } else {
+            hashmap[sortedArray].push(strs[i])
+        }
+    }
+    return Object.values(hashmap)
+};
+
