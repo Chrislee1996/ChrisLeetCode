0e8b78ce1a9153457cb1fce4f1890c1f91e0ebf8
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-09 09:12:03 -0400

    Time: 136 ms, Memory: 44.4 MB - LeetHub

---
+++ number-of-islands/number-of-islands.js
@@ -0,0 +1,33 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    if (grid === null || grid.length === 0 ) {
+        return 0
+    }
+    
+    let numberOfIslands = 0
+    
+    for (let i = 0 ; i < grid.length;i++) {
+        for (let j = 0 ; j < grid[0].length; j++) {
+            if (grid[i][j] === '1') {
+                numberOfIslands += islandCounter(grid , i , j)
+            }
+        }
+    }
+    return numberOfIslands
+};
+
+const islandCounter = (grid, row , col) => {
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === "0") {
+        return
+    }
+    grid[row][col] = "0"
+    islandCounter(grid, row +1, col)
+    islandCounter(grid, row -1, col)
+    islandCounter(grid, row , col +1)
+    islandCounter(grid, row , col -1)
+    return 1
+    
+}
