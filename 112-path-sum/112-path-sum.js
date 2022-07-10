778424271a6a2df64e5efd05176d4b69e9f7376b
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-10 11:25:46 -0400

    Time: 75 ms (90.99%), Space: 45.5 MB (73.57%) - LeetHub

--- 112-path-sum/112-path-sum.js
+++ 112-path-sum/112-path-sum.js
@@ -12,14 +12,14 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-   if (root === null) {
-       return false
-   }
+    if (root === null) {
+        return false
+    }
     
-    if (targetSum === root.val && root.left === null && root.right === null ) {
+    if (targetSum === root.val && root.left === null && root.right === null) {
         return true
     }
     
-    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right , targetSum - root.val)
     
 };
