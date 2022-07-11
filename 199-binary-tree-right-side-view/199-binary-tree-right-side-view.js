10b3cc7ad1d111b7a4a82003eb0fb817d8a22394
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-11 11:30:08 -0400

    Time: 119 ms (14.27%), Space: 44 MB (37.08%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -11,22 +11,20 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    
- 
-    
     let result = []
     let queue = []
     
-       if (root === null) {
+    if (root === null) {
         return []
     }
     
-    queue.push(root) 
-    while (queue.length > 0 ) {
-        let length = queue.length
-        for (let i = 0; i < length ; i++) {
+    queue.push(root)
+    
+    while(queue.length > 0) {
+        let queueLength = queue.length
+        for (let i = 0 ; i < queueLength; i++) {
             let node = queue.shift()
-            if (i === length - 1) {
+            if (i === queueLength-1) {
                 result.push(node.val)
             }
             if (node.left !== null) queue.push(node.left)
@@ -35,3 +33,31 @@ var rightSideView = function(root) {
     }
     return result
 };
+
+
+//[1,2,3,null,5,null,4]
+//queue = [1,2,3,n,5,n,4]
+//1
+
+
+//     let result = []
+//     let queue = []
+    
+//     if (root === null) {
+//         return []
+//     }
+    
+//     queue.push(root)
+    
+//     while(queue.length > 0){
+//         let length = queue.length
+//         for (let i = 0 ; i < length ; i++) {
+//             let node = queue.shift()
+//             if (i === length - 1) {
+//                 result.push(node.val)
+//             }
+//             if (node.left !== null) queue.push(node.left)
+//             if (node.right !== null) queue.push(node.right)
+//         }
+//     }
+//     return result
