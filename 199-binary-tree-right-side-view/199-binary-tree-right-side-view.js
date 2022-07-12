d2aca2f220279208e54633a61782e1a4ac8b6124
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-12 16:54:51 -0400

    Time: 106 ms (29.35%), Space: 44.1 MB (25.24%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -13,16 +13,16 @@
 var rightSideView = function(root) {
     let result = []
     let queue = []
-    
+        
     if (root === null) {
         return []
     }
     
-    queue.push(root)
+    queue.push(root) 
     
-    while(queue.length > 0) {
+    while (queue.length > 0) {
         let queueLength = queue.length
-        for (let i = 0 ; i < queueLength; i++) {
+        for (let i = 0 ; i < queueLength ; i++) {
             let node = queue.shift()
             if (i === queueLength-1) {
                 result.push(node.val)
@@ -35,29 +35,3 @@ var rightSideView = function(root) {
 };
 
 
-//[1,2,3,null,5,null,4]
-//queue = [1,2,3,n,5,n,4]
-//1
-
-
-//     let result = []
-//     let queue = []
-    
-//     if (root === null) {
-//         return []
-//     }
-    
-//     queue.push(root)
-    
-//     while(queue.length > 0){
-//         let length = queue.length
-//         for (let i = 0 ; i < length ; i++) {
-//             let node = queue.shift()
-//             if (i === length - 1) {
-//                 result.push(node.val)
-//             }
-//             if (node.left !== null) queue.push(node.left)
-//             if (node.right !== null) queue.push(node.right)
-//         }
-//     }
-//     return result
