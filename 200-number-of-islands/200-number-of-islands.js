a947b62960b176729382ec674e92b9c015f95f8e
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-13 15:55:36 -0400

    Time: 107 ms (74.02%), Space: 44.9 MB (70.92%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -9,25 +9,25 @@ var numIslands = function(grid) {
     
     let numberOfIslands = 0
     
-    for (let i = 0 ; i < grid.length;i++) {
-        for (let j = 0 ; j < grid[0].length; j++) {
+    for (let i = 0 ; i < grid.length ; i++) {
+        for (let j = 0; j < grid[0].length ; j++) {
             if (grid[i][j] === '1') {
-                numberOfIslands += islandCounter(grid , i , j)
+                numberOfIslands += islandCounter(grid, i , j)
             }
         }
     }
     return numberOfIslands
-};
-
-const islandCounter = (grid, row , col) => {
-    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === "0") {
-        return
-    }
-    grid[row][col] = "0"
-    islandCounter(grid, row +1, col)
-    islandCounter(grid, row -1, col)
-    islandCounter(grid, row , col +1)
-    islandCounter(grid, row , col -1)
-    return 1
-    
 }
+
+
+const islandCounter = (grid , row , col) => {
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === '0') return
+    grid[row][col] = "0"
+    
+    islandCounter(grid, row +1 , col)
+    islandCounter(grid, row -1, col)
+    islandCounter(grid, row , col + 1)
+    islandCounter(grid, row , col - 1)
+    return 1
+}
+
