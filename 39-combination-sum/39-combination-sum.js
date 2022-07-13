a38f6aadd00799a7873315334a27225f2d6a6da4
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-13 17:11:29 -0400

    Time: 107 ms (74.07%), Space: 45.3 MB (72.19%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+        let result = []
+    
+    candidates.sort((a,b) => a-b)
+    
+    function dfs (i, candidates, target, combinations) {
+        // back tracking case - 
+        if (target < 0) return
+        
+        //base case
+        if (target === 0) {
+            result.push(combinations.slice())
+            return
+        }
+        
+        
+        //dfs recursive call
+        for (let j = i ; j < candidates.length ; j++) {
+            combinations.push(candidates[j])
+            dfs(j, candidates, target - candidates[j] , combinations)
+            combinations.pop()
+        }
+    }
+    
+    dfs(0, candidates,target, [])
+    return result
+};
