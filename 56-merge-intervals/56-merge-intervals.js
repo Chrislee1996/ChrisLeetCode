5af668c5c611e75e8237ebf0ead1a691704c8d88
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-13 16:45:01 -0400

    Time: 125 ms (82.74%), Space: 48.4 MB (91.07%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -6,18 +6,46 @@ var merge = function(intervals) {
 
     
     intervals.sort((a,b) => a[0]-b[0])
+    // let result = [intervals[0]]
+    
+//     for (let interval of intervals) {
+//         let endOne = result[result.length-1][1]
+//         let endTwo = interval[1]
+//         let startTwo = interval[0]
+        
+//         if (endOne >= startTwo) {
+//             result[result.length-1][1] = Math.max(endOne, endTwo)
+//         } else {
+//             result.push(interval)
+//         }
+//     }
+//     return result
+    
     let result = [intervals[0]]
     
-    for (let interval of intervals) {
+    for (let i = 1 ; i < intervals.length ; i++) {
         let endOne = result[result.length-1][1]
-        let endTwo = interval[1]
-        let startTwo = interval[0]
-        
-        if (endOne >= startTwo) {
-            result[result.length-1][1] = Math.max(endTwo, endOne)
+        let endTwo = intervals[i][1]
+        let startTwo = intervals[i][0]
+                if (endOne >= startTwo) {
+            result[result.length-1][1] = Math.max(endOne, endTwo)
         } else {
-            result.push(interval)
+            result.push(intervals[i])
         }
     }
-    return result
+    return result  
 };
+
+
+//     for (let interval of intervals) {
+//         let endOne = result[result.length-1][1]
+//         let endTwo = interval[1]
+//         let startTwo = interval[0]
+        
+//         if (endOne >= startTwo) {
+//             result[result.length-1][1] = Math.max(endTwo, endOne)
+//         } else {
+//             result.push(interval)
+//         }
+//     }
+//     return result
