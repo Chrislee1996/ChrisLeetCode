50b157a0616eb2efd2da3b2c148a75b909a0f118
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-13 16:03:59 -0400

    Time: 100 ms (50.22%), Space: 43.7 MB (90.35%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.js
+++ 252-meeting-rooms/252-meeting-rooms.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var canAttendMeetings = function(intervals) {
-    intervals.sort((a,b) =>  a[0] - b[0])
+    intervals.sort((a,b) => a[0] - b[0])
+    
     for (let i = 1 ; i < intervals.length ;i++) {
-        const n1 = intervals[i-1][1]
-        const n2 = intervals[i][0]
-        if (n1 > n2) {
+        let firstMeeting = intervals[i-1][1]
+        let secondMeeting = intervals[i][0]
+        if (firstMeeting > secondMeeting) {
             return false
         }
     }
@@ -16,32 +17,11 @@ var canAttendMeetings = function(intervals) {
 
 
 //[[0,30],[5,10],[15,20]]
-//     n          i
+//    m1   i  
+//               m2
 // [0,30] [5,10] [15,20] 
 //     i          n
 // [2,4]  [7,10]
 //    i    n
 
 
-// [[0,30],[60,240],[90,120]]
-//      n    i
-
-
-//     intervals.sort((a, b) => a.start - b.start);
-    
-//     // can't attend if the next meeting starts before the previous ends
-//     for (let i = 1; i < intervals.length; i++)
-//         if (intervals[i].start < intervals[i-1].end) return false;
-        
-//     return true;
-
-// 	intervals.sort((a, b) => a[0] - b[0]);
-
-// 	for (let i = 1; i < intervals.length; i++) {
-// 		const [, end1] = intervals[i - 1];
-// 		const [start2] = intervals[i];
-
-// 		if (start2 < end1) return false;
-// 	}
-
-// 	return true;
