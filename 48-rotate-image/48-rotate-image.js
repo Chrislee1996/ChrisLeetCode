0182e0abfc868bbc2ceff2555af23ba9c14a9925
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-15 12:56:29 -0400

    Time: 94 ms (39.47%), Space: 41.9 MB (84.85%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -11,11 +11,14 @@ var rotate = function(matrix) {
         }
     }
     
-    for (let i = 0 ; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length/2 ; j++) {
-            let temp = matrix[i][j]
-            matrix[i][j] = matrix[i][matrix.length-j-1]
-            matrix[i][matrix.length-j-1] = temp
-        }
+    // for (let i = 0 ; i < matrix.length; i++) {
+    //     for (let j = 0; j < matrix[0].length/2 ; j++) {
+    //         let temp = matrix[i][j]
+    //         matrix[i][j] = matrix[i][matrix.length-j-1]
+    //         matrix[i][matrix.length-j-1] = temp
+    //     }
+    // }
+    for (let row of matrix){
+        row.reverse()
     }
 };
