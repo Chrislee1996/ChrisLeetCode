bb70ac279411f442c316287a2035dd37ada8b6e5
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-15 12:35:06 -0400

    Time: 453 ms (85.50%), Space: 42.8 MB (71.52%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -4,55 +4,36 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-
-        for (let i = 0 ; i < board.length;i++) {
+    for (let i = 0 ; i < board.length; i++) {
         for (let j = 0 ; j < board[0].length; j++) {
-            if (board[i][j] === word[0] && dfsHelper(i,j,0)) return true
+            if (board[i][j] === word[0] && wordSearch(i ,j, 0)) return true
         }
     }
     return false
-    
-    function dfsHelper(row , col , index) {
-        if (word.length === index) return true
-        
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] !== word[index]) return 
-        
-        board[row][col] = '#'
-        
-        if (dfsHelper(row+1, col, index+1) ||
-        dfsHelper(row-1, col, index+1) ||
-        dfsHelper(row, col+1, index+1) ||
-        dfsHelper(row, col-1, index+1)) return true
-        
-        board[row][col] = word[index]
 
+            function wordSearch(row , col, index){
+            if (word.length === index) return true
+            if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] !== word[index]) return
+            board[row][col] = null
+                
+                if (wordSearch(row + 1, col, index+1) ||
+                wordSearch(row -1, col, index+1) ||
+                wordSearch(row, col + 1 , index+1) ||
+                wordSearch(row, col -1 , index+1)) return true
+                    
+                board[row][col] = word[index]
     }
-    
-
 };
 
 
-// var exist = function(board, word) {
-//     const ROW_NUM = board.length, COL_NUM = board[0].length;
-    
-//     function callDFS(r, c, idx) {
-//         if(word.length === idx) return true;
-//         if(r >= ROW_NUM || r < 0 || board[r][c] !== word[idx]) return false; 
-        
-//         board[r][c] = '#'; // mark as visited
-        
-//         if (callDFS(r+1, c, idx+1)||
-//             callDFS(r-1, c, idx+1)||
-//             callDFS(r, c+1, idx+1)||
-//             callDFS(r, c-1, idx+1)) return true;
-            
-//         board[r][c] = word[idx]; // reset the board
-//     }
-    
-//     for(let r = 0; r < ROW_NUM; r++) {
-//         for(let c = 0; c < COL_NUM; c++) {
-//             if(board[r][c] === word[0] && callDFS(r, c, 0)) return true;
-//         }
-//     }
-//     return false;    
-// };
+
+//loop through the 2d array
+//compare our board with the 0index word 
+//if it matches - we can do our dfs call
+//if not we can just return false
+
+
+//in our dfs - check boundries
+//make sure to mark if a grid is checked
+//if a word matches, we can do our recur call
+//reset board 
