5d2e4462e436fb93d3e3ed4a6a352bc1c9bd4292
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-15 12:02:47 -0400

    Time: 120 ms (56.48%), Space: 44.8 MB (80.61%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -3,47 +3,21 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    let maxIsland = 0
-    for (let i = 0 ; i < grid.length ;i++) {
-        for (let j = 0 ; j < grid[0].length; j++) {
-            maxIsland = Math.max(maxIsland, islandCounter(grid, i , j))
+    let maxIslandArea = 0
+    for (let i = 0 ; i < grid.length ; i++) {
+        for (let j = 0 ; j < grid[0].length ; j++) {
+            maxIslandArea = Math.max(maxIslandArea, islandCounter(grid, i , j))
         }
     }
-    return maxIsland 
+    return maxIslandArea
 };
 
-function islandCounter(grid, row, col) {
-    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) {
-        return 0
-    }
+const islandCounter = (grid, row, col) => {
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) return 0
     grid[row][col] = 0
-    let up = islandCounter(grid, row +1,col)
-    let down = islandCounter(grid, row -1,col)
-    let right = islandCounter(grid, row,col +1)
-    let left = islandCounter(grid, row,col -1)
-
-    return 1 + up + down + right + left
+    let up = islandCounter(grid, row , col+1)
+    let down = islandCounter(grid, row , col-1)
+    let left = islandCounter(grid, row -1 , col)
+    let right = islandCounter(grid, row +1 , col)
+    return 1 + up + down + left + right
 }
-
-
-//  var maxScore = 0
-//     for (var r = 0; r < grid.length; r++){
-//         for (var c = 0; c < grid[0].length; c++){
-//             maxScore = Math.max(maxScore, DFS(grid,r,c))
-//         }
-//     }
-//     return maxScore
-// };
-
-// function DFS(grid, row, col){
-    
-//     if (row >= grid.length || col >= grid[0].length || col < 0 || row < 0 || grid[row][col] != 1) return 0
-
-//     grid[row][col] = 2;
-    
-//     var up = DFS(grid, row + 1, col)
-//     var down = DFS(grid, row - 1, col)
-//     var right = DFS(grid, row, col + 1)
-//     var left = DFS(grid, row, col - 1)
-    
-//     return 1 + up + down + left + right
