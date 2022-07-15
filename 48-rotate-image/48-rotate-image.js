77c83a9489682cca7d4a396eb50f80421dad9d02
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-15 12:49:28 -0400

    Time: 84 ms (58.20%), Space: 42.2 MB (57.57%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -3,16 +3,7 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-        // [1,2,3],    
-        // [4,5,6],
-        // [7,8,9]
-    
-    
-        // [1,4,7],    
-        // [2,5,8],
-        // [3,6,9]
-    
-    for (let i = 0 ; i < matrix.length ; i++ ) {
+    for (let i = 0 ; i < matrix.length ; i++) {
         for (let j = i ; j < matrix[0].length ; j++) {
             let temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
@@ -20,19 +11,11 @@ var rotate = function(matrix) {
         }
     }
     
-        // [1,4,7],    
-        // [2,5,8],
-        // [3,6,9]
-    
-        // [7,4,1],    
-        // [8,5,2],
-        // [9,6,3]
-    
-    for (let i = 0 ; i < matrix.length ; i++) {
-        for (let j = 0;  j < matrix[0].length/2;j++) {
+    for (let i = 0 ; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length/2 ; j++) {
             let temp = matrix[i][j]
-            matrix[i][j] = matrix[i][matrix[0].length-j-1]
-            matrix[i][matrix[0].length-j-1]  = temp
+            matrix[i][j] = matrix[i][matrix.length-j-1]
+            matrix[i][matrix.length-j-1] = temp
         }
     }
 };
