ae46766a18279c240d85ab147f9057222259f215
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-15 13:11:44 -0400

    Time: 89 ms (66.12%), Space: 44 MB (76.21%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.js
+++ 252-meeting-rooms/252-meeting-rooms.js
@@ -4,24 +4,12 @@
  */
 var canAttendMeetings = function(intervals) {
     intervals.sort((a,b) => a[0] - b[0])
-    
-    for (let i = 1 ; i < intervals.length ;i++) {
+    for (let i = 1 ; i < intervals.length; i++) {
         let firstMeeting = intervals[i-1][1]
-        let secondMeeting = intervals[i][0]
-        if (firstMeeting > secondMeeting) {
+        let nextMeeting = intervals[i][0]
+        if (firstMeeting>nextMeeting) {
             return false
         }
     }
     return true
 };
-
-
-//[[0,30],[5,10],[15,20]]
-//    m1   i  
-//               m2
-// [0,30] [5,10] [15,20] 
-//     i          n
-// [2,4]  [7,10]
-//    i    n
-
-
