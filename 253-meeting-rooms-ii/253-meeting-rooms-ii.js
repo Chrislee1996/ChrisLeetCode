ce7e399bc3f6bb7e2cc6be154c12bfef22ff6291
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-17 09:33:58 -0400

    Time: 104 ms (64.90%), Space: 44.1 MB (91.05%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    let rooms = 0
-    let end = 0
+    let numberOfRooms = 0
+    let endingRooms = 0
     let startTime = intervals.map(a => a[0]).sort((a,b) => a-b)
     let endTime = intervals.map(a => a[1]).sort((a,b) => a-b)
-    for (let i = 0 ; i < intervals.length; i++) {
-        if (startTime[i] < endTime[end]) {
-            rooms++
+    for (let i = 0 ; i < intervals.length;i++) {
+        if (startTime[i] < endTime[endingRooms]) {
+            numberOfRooms++
         } else {
-            end++
+            endingRooms++
         }
     }
-    return rooms
+    return numberOfRooms
 };
