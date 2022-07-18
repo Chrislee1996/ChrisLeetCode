3e81d45d2692f6d7c66fe23b2383d22f93c37b54
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-18 11:27:44 -0400

    Time: 158 ms (21.30%), Space: 45.3 MB (77.73%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -6,8 +6,7 @@
 var combinationSum = function(candidates, target) {
     let result = []
     
-    function dfsHelper(candidates, target, i,  combinations) {
-        //base case
+    function dfsHelper(candidates, target, idx, combinations) {
         if (target < 0) return
         
         if (target === 0 ) {
@@ -15,37 +14,13 @@ var combinationSum = function(candidates, target) {
             return
         }
         
-        for (let j = i ; j < candidates.length; j++) {
+        for (let j = idx ; j < candidates.length ; j++) {
             combinations.push(candidates[j])
-            dfsHelper(candidates, target - candidates[j] ,j, combinations)
+            dfsHelper(candidates, target - candidates[j], j, combinations)
             combinations.pop()
         }
-        
     }
-    dfsHelper(candidates,target, 0, [])
+    
+    dfsHelper(candidates, target, 0 , [])
     return result
 };
-
-
-// var combinationSum = function(candidates, target) {
-//     let result = []
-    
-//     function dfsHelper(candidates, target, i,  combinations) {
-//         //base case
-//         if (target < 0) return
-        
-//         if (target === 0 ) {
-//             result.push(combinations.slice())
-//             return
-//         }
-        
-//         for (let j = i ; j < candidates.length; j++) {
-//             combinations.push(candidates[j])
-//             dfsHelper(candidates, target - candidates[j] ,j, combinations)
-//             combinations.pop()
-//         }
-        
-//     }
-//     dfsHelper(candidates,target, 0, [])
-//     return result
-// };
