a1a144d8137237d6939bab11cd5386d747a5a7db
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-18 15:33:11 -0400

    Time: 152 ms (20.47%), Space: 49.5 MB (24.39%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -13,13 +13,17 @@
  */
 var isSubtree = function(root, subRoot) {
     if (root === null) return false
-    if (isSameTree(root, subRoot)) return true
+    if (isSameTree(root,subRoot)) return true
     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
 };
 
-const isSameTree = (root, subRoot) => {
+const isSameTree = (root,subRoot) => {
     if (root === null && subRoot === null) return true
     if (root === null || subRoot === null) return false
     if (root.val !== subRoot.val) return false
     return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right)
 }
+
+//base case
+//check if left or right of our tree equals the subroot
+//need to also check if we only have a left and right and root if that equals the subroot
