38f653bc12638278188961aeb836e9d9a1d78aeb
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-18 11:36:36 -0400

    Time: 75 ms (91.00%), Space: 43.8 MB (90.21%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.js
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -4,56 +4,25 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-    candidates.sort((a,b) => a-b)
-    const result = []
+    candidates.sort((a,b) => a- b)
+    let result = []
     
-    function dfsHelper(candidates, target, i, combinations) {
+    function dfsHelper(candidates, target, idx, combinations) {
         if (target < 0) return
-        
-        
-        if (target === 0) {
+        if (target === 0 ) {
             result.push(combinations.slice())
+            return
         }
         
-        for (let j = i ; j < candidates.length ; j++) {
-            if (i !== j && candidates[j] === candidates[j - 1] ) continue
+        for (let j = idx ; j < candidates.length ; j++) {
+            if (idx !== j && candidates[j] === candidates[j-1]) continue
             combinations.push(candidates[j])
-            dfsHelper(candidates, target - candidates[j] , j + 1, combinations)
+            dfsHelper(candidates, target - candidates[j] , j+1, combinations)
             combinations.pop()
         }
         
-        
     }
+    
     dfsHelper(candidates, target, 0, [])
     return result
 };
-
-
-
-//     let result = []
-    
-//     candidates.sort((a,b) => a-b)
-    
-//     function dfs (i, candidates, target, combinations) {
-//         // back tracking case - 
-//         if (target < 0) return
-        
-//         //base case
-//         if (target === 0) {
-//             result.push(combinations.slice())
-//             return
-//         }
-        
-        
-//         //dfs recursive call
-//         for (let j = i ; j < candidates.length ; j++) {
-//             if (i !== j && candidates[j] === candidates[j-1]) continue
-//             combinations.push(candidates[j])
-//             dfs(j + 1, candidates, target - candidates[j] , combinations)
-//             combinations.pop()
-//         }
-//     }
-    
-//     dfs(0, candidates,target, [])
-//     return result
-    
