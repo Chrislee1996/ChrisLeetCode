536af08b889575cecca58de3894e3dcc40b323d5
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-18 20:14:15 -0400

    Time: 77 ms (64.24%), Space: 42.4 MB (18.74%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    let result = []
+    let letterToNumbers = {
+        '2' : 'abc',
+        '3' : 'def',
+        '4' : 'ghi',
+        '5' : 'jkl',
+        '6' : 'mno',
+        '7' : 'pqrs',
+        '8' : 'tuv',
+        '9' : 'wxyz'
+    }
+    
+    if (digits.length === 0) return result
+    
+    
+    function dfsHelper(digits, idx, combinations) {
+        if (idx === digits.length) {
+            result.push(combinations.join(''))
+            return
+        }
+        
+        let chars = letterToNumbers[digits[idx]]
+        
+        for (let char of chars) {
+            combinations.push(char)
+            dfsHelper(digits, idx + 1 , combinations)
+            combinations.pop()
+        }
+    }
+    
+    dfsHelper(digits, 0 , [])
+    return result
+};
