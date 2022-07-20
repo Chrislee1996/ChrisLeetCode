67d36814a622d663916702791b8df6d54e54c9a0
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-20 15:01:34 -0400

    Time: 79 ms (77.74%), Space: 44.2 MB (63.23%) - LeetHub

--- 1436-destination-city/1436-destination-city.js
+++ 1436-destination-city/1436-destination-city.js
@@ -3,12 +3,18 @@
  * @return {string}
  */
 var destCity = function(paths) {
-    let set = new Set()
-    for (let path of paths) {
-        set.add(path[0])
-    }
+//     let set = new Set()
+//     for (let path of paths) {
+//         set.add(path[0])
+//     }
+
+//     for (let path of paths) {
+//         if (!set.has(path[1])) return path[1]
+
+//     }
     
-    for (let path of paths) {
-        if (!set.has(path[1])) return path[1]
-    }
+     const hash = new Map(paths);
+      for(const places of hash.values()) {
+        if (!hash.has(places)) return places;
+  }
 };
