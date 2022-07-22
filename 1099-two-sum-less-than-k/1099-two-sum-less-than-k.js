2bfde6d1eea0ca9bc62c20cf5c6e898292646278
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-22 15:33:47 -0400

    Time: 95 ms (47.67%), Space: 42.1 MB (85.27%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.js
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.js
@@ -4,19 +4,33 @@
  * @return {number}
  */
 var twoSumLessThanK = function(nums, k) {
+    // let max = -1
+    // for (let i = 0 ; i< nums.length ; i++) {
+    //     for (let j = i+ 1; j < nums.length ; j++) {
+    //         let sum = nums[i] + nums[j]
+    //         if (sum < k && sum > max) max = sum
+    //     }
+    // }
+    // return max
+    
+    
+    nums.sort((a,b) => a-b)
+    let leftPointer = 0
+    let rightPointer = nums.length-1
     let max = -1
-    for (let i = 0 ; i< nums.length ; i++) {
-        for (let j = i+ 1; j < nums.length ; j++) {
-            let sum = nums[i] + nums[j]
-            if (sum < k && sum > max) max = sum
+    
+    while (leftPointer < rightPointer ) {
+        if (nums[leftPointer] + nums[rightPointer] < k ){
+            max = Math.max(nums[leftPointer] + nums[rightPointer], max)
+            leftPointer++
+        } else {
+            rightPointer--
         }
     }
     return max
 };
 
 
-//34,23,1,24,75,33,54,8
-//   O                T
-
-    //42
-    //31
+// [34,23,1,24,75,33,54,8]
+//  1 8 23 24 33 34 54 75 
+//         o      o
