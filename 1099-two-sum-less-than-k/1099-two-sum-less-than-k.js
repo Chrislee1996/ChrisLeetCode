452ce865ccb707c12ceb7a5a9da1651d217f3d18
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-22 15:22:53 -0400

    Time: 92 ms (52.71%), Space: 41.8 MB (96.51%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.js
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.js
@@ -5,7 +5,7 @@
  */
 var twoSumLessThanK = function(nums, k) {
     let max = -1
-    for (let i = 0 ; i< nums.length-1 ; i++) {
+    for (let i = 0 ; i< nums.length ; i++) {
         for (let j = i+ 1; j < nums.length ; j++) {
             let sum = nums[i] + nums[j]
             if (sum < k && sum > max) max = sum
