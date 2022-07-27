f18ee5ee32f12235aeb9114a30242e2bb4e37743
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-27 15:59:04 -0400

    Time: 115 ms (78.13%), Space: 43.9 MB (95.57%) - LeetHub

---
+++ 18-4sum/18-4sum.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    nums.sort((a,b) => a-b)
+    
+    let result = []
+    
+    for (let i = 0 ; i < nums.length-3 ; i++) {
+        for (let j = i + 1; j < nums.length-2; j++) {
+            let leftPointer = j + 1
+            let rightPointer = nums.length-1
+            
+            
+            while (leftPointer < rightPointer) {
+                const sum = nums[i] + nums[j] + nums[leftPointer] + nums[rightPointer]
+                if (sum === target) {
+                    result.push([nums[i] ,nums[j] ,nums[leftPointer] ,nums[rightPointer]])
+                    while (nums[leftPointer] === nums[leftPointer+1]) leftPointer++
+                    while (nums[rightPointer] === nums[rightPointer-1]) rightPointer--
+                    leftPointer++
+                    rightPointer--
+                } else if (sum < target) {
+                    leftPointer++
+                } else {
+                    rightPointer--
+                }
+            }
+            while(nums[j] === nums[j + 1]) j++;
+        }   
+        while(nums[i] === nums[i + 1]) i++;
+    }
+    return result
+};
+
