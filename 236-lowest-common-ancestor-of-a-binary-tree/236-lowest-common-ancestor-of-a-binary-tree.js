61fa825e5c8a707f0d299558da19d5eecfe59728
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-29 13:08:41 -0400

    Time: 127 ms (42.29%), Space: 52.1 MB (26.07%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,9 +12,9 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if (root === null || root === p || root === q) {
-        return root
-    }
+    if (root === null) return null
+    
+    if (root === p || root === q) return root 
     
     const left = lowestCommonAncestor(root.left, p , q)
     const right = lowestCommonAncestor(root.right,p,q)
@@ -31,3 +31,9 @@ var lowestCommonAncestor = function(root, p, q) {
  //    if (!left) return right  // p and q are in the right subtree
  //    if (!right) return left  // p and q are in the left subtree
  //    return root        
+
+//base case for recur for checking null cases
+//if at any time our p or q equals the root we return the root
+//traverse through our left and right side
+//if we do not have a left we return right and viceversa
+//return root
