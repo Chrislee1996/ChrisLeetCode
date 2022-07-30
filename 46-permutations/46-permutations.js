9b3471761d6c56eb89d917e79a190328aabcbc5d
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-29 21:05:54 -0400

    Time: 91 ms (78.21%), Space: 45.4 MB (24.32%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -3,19 +3,34 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    if (nums.length === 0 ) return [[]]
     let result = []
     
-    let firstNumber = nums[0]
-    let otherNumbers = nums.slice(1)
-    
-    let allOtherNumbers = permute(otherNumbers)
-    
-    for (let numbers of allOtherNumbers) {
-        for (let i = 0 ; i <= numbers.length ; i++) {
-            let allNumbers = [...numbers.slice(0,i), firstNumber, ...numbers.slice(i)]
-            result.push(allNumbers)
+    const dfs = (i, nums) => {
+        if (nums.length === 0 ) return [[]]
+        
+        if (i === nums.length) {
+            result.push(nums.slice())
+            return
+        }
+        
+        for (let j = i ; j < nums.length; j++) {
+            // [nums[i], nums[j]] = [nums[j], nums[i]]
+            // dfs(i + 1, nums)
+            // [nums[i], nums[j]] = [nums[j], nums[i]]
+            // swap the positions
+            var temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            dfs(i + 1, nums);
+            // unswap the positions as we go back up the tree
+            nums[j] = nums[i];
+            nums[i] = temp;
         }
     }
+    
+    dfs(0,nums)
     return result
 };
+
+//time = n!
+//space = n^2
