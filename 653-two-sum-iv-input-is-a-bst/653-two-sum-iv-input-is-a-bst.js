2fbcb35c6a653019e4eb0ca48af10545b9333004
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-30 17:30:08 -0400

    Time: 137 ms (63.18%), Space: 52.8 MB (10.84%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
@@ -16,18 +16,33 @@ var findTarget = function(root, k) {
         return false
     }
     
-      const set = new Set();
+      const set = new Map();
       const stack = [root];
       while (stack.length > 0) {
         const node = stack.pop();
-        if (set.has(k - node.val)) {
-            return true;
-        }
-        set.add(node.val);
-                  if (node.left) stack.push(node.left);
-
+        if (set.has(k - node.val)) return true;
+        set.set(node.val);
+        if (node.left) stack.push(node.left);
         if (node.right) stack.push(node.right);
       }
       return false;
+    
+    
 
 };
+
+
+
+//if we traverse and get null, return false
+//create a new set and a stack
+//we will then pop our stack
+//we will then subtract that we just popped with our k value
+//if that value is found within our set, we can return true
+//add that popped number to our set
+//continue via left and right
+//if we have finished our traversal just return false
+
+//ex- 
+// stack will go 5-3-2-4
+//when we reach 4, it will subtract from 9 which will return 5
+//5 is found within our stack so return true
