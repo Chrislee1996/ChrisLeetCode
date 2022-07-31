3f8fa0ac2c0462415f5a25dacaa79afd889baa51
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-07-31 11:18:59 -0400

    Time: 132 ms (34.51%), Space: 46.8 MB (78.76%) - LeetHub

---
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var getLonelyNodes = function(root) {
+
+    const result = []
+    
+    const dfs = (node) => {
+        if (node === null) return 
+    
+        if (node.left === null && node.right) result.push(node.right.val)
+        if (node.right === null && node.left) result.push(node.left.val)
+        
+        dfs(node.left)
+        dfs(node.right)
+        
+    }
+    
+    
+    dfs(root)
+    return result
+};
+
+// const getLonelyNodes = (root) => {
+//     let lonely = []
+//     const dfs = root => {
+//         if (!root) return
+//         if (!root.left && root.right) lonely.push(root.right.val)
+//         if (!root.right && root.left) lonely.push(root.left.val)   
+//         dfs(root.left)
+//         dfs(root.right)
+//     }
+//     dfs(root)
+//     return lonely
+// };
