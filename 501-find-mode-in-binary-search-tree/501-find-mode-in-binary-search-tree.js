25f3456838cd03abd56579a3c4cb2f5014c8e361
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-08-01 11:33:52 -0400

    Time: 124 ms (56.06%), Space: 50.5 MB (50.87%) - LeetHub

---
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findMode = function(root) {
+    if (root === null) return
+    let mode = []
+    let hashmap = {}
+    let compareValue = -Infinity
+    
+    const dfs = (node) => {
+        if (node === null) return
+        
+        if (!hashmap[node.val]) {
+            hashmap[node.val] = 1
+        } else {
+            hashmap[node.val]++
+        }
+        
+        if (hashmap[node.val] > compareValue) compareValue = hashmap[node.val]
+        
+        dfs(node.left)
+        dfs(node.right)
+        
+    }
+    
+    dfs(root)
+    Object.keys(hashmap).forEach(modeValue => {if(hashmap[modeValue] == compareValue) mode.push(modeValue)});
+    return mode
+};
