5f41204fab96f029c1f5a1f8718f103b44b67e93
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-08-01 17:34:23 -0400

    Time: 67 ms (100.00%), Space: 52.2 MB (40.68%) - LeetHub

---
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+    let successor = null
+    
+    const search = (node) => {
+        if (node === null) return null
+        if (p.val >= node.val) {
+            search(node.right)
+        } else {
+            successor = node
+            search(node.left)
+        }
+    }
+    search(root)
+    return successor
+};
+
+
+
+    // let successor = null;
+    
+//     const search = (node) => {
+//         if (!node) return null;
+        
+//         if (p.val >= node.val) {
+//             search(node.right);
+//         } else {
+//             successor = node;
+//             search(node.left);
+//         }
+//     }
+//     search(root);
+//     return successor;
