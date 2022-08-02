471cca5ccc1ca52d1f7d445ca233213660a9826a
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-08-02 12:50:16 -0400

    Time: 123 ms (38.31%), Space: 47.4 MB (45.05%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.js
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -11,24 +11,27 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    if (root === null) {
-        return true
+    if (root === null) return null
+    
+    const dfs = (root) => {
+        if (root === null) return 0
+        let left = dfs(root.left) + 1
+        let right = dfs(root.right) + 1
+        if (Math.abs(left - right) > 1 ) return Infinity
+        return Math.max(left,right)
     }
     
-    const dfs = (node) => {
-        if (node === null) {
-            return 0
-        }
-        
-        let leftSide = dfs(node.left)
-        let rightSide = dfs(node.right)
-        
-        if (leftSide === -1 || rightSide === -1 || Math.abs(leftSide-rightSide) > 1 ) {
-            return -1
-        }
-        return Math.max(leftSide,rightSide) + 1
+    if (dfs(root) === Infinity) {
+        return false
     }
-    
-    return dfs(root) !== -1
+    return true
     
 };
+
+//create base case for null nodes
+//traverse the left and right side
+// if the left or right side is unbalanced return -1 
+//also, if our side is more unbalanced than the other side, and it is greater than 1 (per the problem) then we need to also return -1
+//return the math max of the left and right side + 1
+//if our result !== -1 then we know it is balanced
+ // return dfs(root)==Infinity?false:true;
