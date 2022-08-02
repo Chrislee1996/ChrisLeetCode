ad3ddc89b4b7f3fd0609dc3237abec568a1f1b19
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-08-02 12:28:56 -0400

    Time: 130 ms (23.42%), Space: 45.1 MB (7.22%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -13,10 +13,20 @@
 var sortedArrayToBST = function(nums, left = 0 , right = nums.length-1) {
     if (left > right) return null
     
-    let mid = Math.floor((left + right) / 2)
-    let root = new TreeNode(nums[mid])
-    root.left = sortedArrayToBST(nums, left , mid -1)
-    root.right = sortedArrayToBST(nums, mid + 1, right)
+    let middle = Math.floor((left + right) /2 )
+    let headRoot = new TreeNode(nums[middle])
+    headRoot.left = sortedArrayToBST(nums, left, middle -1)
+    headRoot.right = sortedArrayToBST(nums, middle+1, right)
     
-    return root
+    return headRoot
+    
 };
+
+
+//set our left side of the tree to 0 index of our array
+//if our left side is ever greater than the right than we know we reached the end  of our array
+//set our right side to the last digit in our nums array
+//create a mid point since we know that the middle of our array is always the root node
+//traverse our left side -> left side will be the left side of  the middle of our array - 1
+//right is the right side of our mid array + 1
+//return root
