bfc21a6527e5e9d326d7221d726f900285a290a3
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-08-04 12:37:39 -0400

    Time: 116 ms (38.59%), Space: 48.3 MB (20.65%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.js
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.js
@@ -14,29 +14,19 @@
  */
 var trimBST = function(root, low, high) {
     if (root === null) return null
-    
-    if (root.val < low) return trimBST(root.right, low,high)
-    if (root.val > high) return trimBST(root.left, low,high)
+    if (root.val < low) return trimBST(root.right, low, high)
+    if (root.val > high) return trimBST(root.left, low, high)
     
     root.left = trimBST(root.left, low, high)
-    root.right = trimBST(root.right, low, high)
-    
+    root.right = trimBST(root.right, low, high)  
     return root
 };
 
-
-// if (root === null) {
-//         return null;
-//     }
-//     if (root.val > R) {
-//         // skip and go left
-//         return trimBST(root.left, L, R);
-//     }
-//     if (root.val < L) {
-//         // skip and go right
-//         return trimBST(root.right, L, R);
-//     }
-//     // connect left and right child to the next qualified node
-//     root.left = trimBST(root.left, L, R);
-//     root.right = trimBST(root.right, L, R);
-//     return root;  
+//Since this is a BST, we know that the left side of our tree is less than the root and the right is greater than our root
+//Since we also know the low and high, we can just compare the current root.val with the low and high
+//IF our current root value is less than the low it is out of bounds => we can traverse to the right
+    //for example, in our 2nd example, 0 < 1 so we know there is no point in checking our left side since it is less
+//IF our current root value is greater than the low it is out of bounds => we can traverse to the left
+    //again, if our root was 3 and the low was 4 and high was 5 we can just ignore the right and go left
+// if none of these events occur, we can just traverse left/right to continue our checking
+//return root
