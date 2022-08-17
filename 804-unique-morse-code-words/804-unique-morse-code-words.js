6926107252d58e1ebc6d777a6b7512b7c2009535
Christopher Lee <88732134+Chrislee1996@users.noreply.github.com>
2022-08-17 12:33:17 -0400

    Time: 140 ms (5.47%), Space: 43.9 MB (37.23%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+     var morse = [".-","-...","-.-.","-..",".","..-.",
+                  "--.","....","..",".---",
+                  "-.-",".-..","--","-.","---",".--.",
+                  "--.-",".-.","...","-","..-","...-",
+                  ".--","-..-","-.--","--.."];
+    
+    var transformations = new Set();
+    
+    for (let word of words) {        
+        var trans = "";
+        for (let letter of word) {
+            var index = letter.charCodeAt(0) - 97;
+            trans += morse[index];
+        }
+        
+        transformations.add(trans);   
+    }
+    
+    return transformations.size;
+};
